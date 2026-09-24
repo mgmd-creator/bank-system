12d2236e0a5451b14b577f572a83bd7aa7ed8e49
mgmd-creator <ibragimovmagomed900@gmail.com>
2026-09-24 18:42:15 +0300

    feat: implement Main console interface

--- src/main/java/com/bank/exception/InsufficientFundsException.java
+++ src/main/java/com/bank/exception/InsufficientFundsException.java
@@ -4,22 +4,7 @@ package com.bank.exception;
  * Исключение, выбрасываемое при попытке снять сумму, превышающую текущий баланс счета.
  */
 public class InsufficientFundsException extends BankException {
-    public InsufficientFundsException() {
-    }
-
     public InsufficientFundsException(String message) {
         super(message);
-    }
-
-    public InsufficientFundsException(String message, Throwable cause) {
-        super(message, cause);
-    }
-
-    public InsufficientFundsException(Throwable cause) {
-        super(cause);
-    }
-
-    public InsufficientFundsException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
-        super(message, cause, enableSuppression, writableStackTrace);
     }
 }
